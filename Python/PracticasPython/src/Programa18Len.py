c2bf06384e7e5c9c0b641589c2687a401499601d
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-08 13:46:05 -0500

    Python

---
+++ Python/PracticasPython/src/Programa18Len.py
@@ -0,0 +1,15 @@
+#Función len()
+
+# En Python, la función len() nos
+# permite obtener la longitud de una
+# cadena de caracteres, o bien, el
+# número de elementos que componen a
+# un objeto.
+
+#Opción 1
+print("Hola tiene", len("Hola"), "caracteres.")
+
+#Opción 2
+longitud = len("Lexz Temptation")
+print("Lexz Temptation", longitud, "caracteres")
+
