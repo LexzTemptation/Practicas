446ba4aa93136b6cb0e4a4ac68b9450fb7e51a07
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-09 14:31:35 -0500

    Python

--- Python/PracticasPython/src/Programa1Strings.py
+++ Python/PracticasPython/src/Programa1Strings.py
@@ -87,12 +87,13 @@ print()
 # Ejemplo2
 nombre = "Carlos"
 estatura = 1.80
-edad = 2
+edad = 22
 
 print(f"Hola {nombre} tienes {edad} años y mides {estatura} centímetros.")
 print()
 
 # Ejemplo3
+'''
 nombre = input("¿Cúal es tu nombre?: ")
 num_uno = int(input("Introduce un número: "))
 num_dos = int(input("Introduce un segundo número: "))
@@ -100,8 +101,60 @@ num_dos = int(input("Introduce un segundo número: "))
 print()
 
 print(f"Hola {nombre} el resultado de {num_uno} + {num_dos} es: {num_uno + num_dos}")
+'''
 
 # ********** Concatenación con método "strip()" **********
+# El método strip , se utiliza para eliminar caracteres especificados al
+# INICIO Y AL FINAL de una cadena de caracteres, tomando en cuenta que
+# si al método strip no se le especifica uno o más caracteres a eliminar,
+# solo eliminara espacios en blanco y saltos de línea.
+print("strip()")
+
+cadena = "\tHola Lexz\n" # \t = tabulación de 4 espacios
+print(cadena)
+
+cadena = cadena.strip("H xz\t\n")
+# Hace comprobaciones hasta que quite las letras especificadas al inicio
+# o al final de la cadena
+print(cadena)
+
+print()
+
+# ********** Concatenación con método "rstrip()" **********
+# El método rstrip(), se utiliza para eliminar únicamente
+# caracteres especificados al FINAL de una cadena.
+# Al igual que el método strip(), si no se especifica uno o más
+# caracteres a eliminar, solo eliminara espacios en blanco y saltos de línea.
+print("rstrip()")
+
+cadena = "\tHola Lexz\n"
+print(cadena)
+
+cadena = cadena.rstrip(" xz\t\n")
+print(cadena)
+
+print()
+
+# ********** Concatenación con método "lstrip()" **********
+# El método Istripo, se utiliza para eliminar únicamente
+# caracteres especificados al INICIO de una cadena.
+# Al igual que los dos métodos anteriores, si no se especifica uno o más
+# caracteres a eliminar, solo eliminara espacios en blanco y saltos de línea.
+print("lstrip()")
+
+cadena = "\tHola Lexz\n"
+print(cadena)
+
+cadena = cadena.lstrip("Hol \t\n")
+print(cadena)
+
+print()
+
+# ********** Concatenación con método "istitle()" y "title()" **********
+# En Python, es posible convertir la primera letra de cada
+# palabra de un String en mayúsculas, y a su vez, convertir el
+# resto de letras que componen a cada palabra de un String en minúsculas.
+
 
 
 
