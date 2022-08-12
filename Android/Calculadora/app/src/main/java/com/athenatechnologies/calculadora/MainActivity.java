1988e66276a5c0dc3f0efba0d72742e0f0df6300
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-12 10:20:22 -0500

    diagrama ER gimnasio terminado (o eso supongo)

--- Android/Calculadora/app/src/main/java/com/athenatechnologies/calculadora/MainActivity.java
+++ Android/Calculadora/app/src/main/java/com/athenatechnologies/calculadora/MainActivity.java
@@ -3,12 +3,48 @@ package com.athenatechnologies.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button suma;
+    private Button resta;
+    private Button multiplicaicion;
+    private Button division;
+    private TextView num1;
+    private TextView num2;
+    private TextView resultado;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        resultado  = findViewById(R.id.resultado);
+        suma = findViewById(R.id.suma);
     }
+
+    private void suma(View view)
+    {
+        float resultadoNum = Float.parseFloat(num1.getText().toString()) + Float.parseFloat(num2.getText().toString());
+        resultado.setText(resultadoNum + " ");
+    }
+    private void resta(View view)
+    {
+        float resultado = Float.parseFloat(num1.getText().toString()) - Float.parseFloat(num2.getText().toString());
+    }
+    private void mult(View view)
+    {
+        float resultado = Float.parseFloat(num1.getText().toString()) * Float.parseFloat(num2.getText().toString());
+    }
+    private void div(View view)
+    {
+        float resultado = Float.parseFloat(num1.getText().toString()) / Float.parseFloat(num2.getText().toString());
+    }
+
 }
