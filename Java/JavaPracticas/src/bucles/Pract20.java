68940a1021ad354a8022a3c6edcd1b5928adf76f
Alejandro Cornejo <alejandro_temptation@outlook.com>
2022-08-11 21:52:35 -0500

    Java formateado Pract20 desde guanalap

--- Java/JavaPracticas/src/bucles/Pract20.java
+++ Java/JavaPracticas/src/bucles/Pract20.java
@@ -9,23 +9,23 @@ import javax.swing.JOptionPane;
 public class Pract20
 {
     public static void main(String[] args)
-    { 
+    {
         System.out.println();
-        
+
         int factorial = 1, num;
 
         num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero a calcular su factorial: "));
 
-        for(int i = 1; i <= num; i++)
+        for (int i = 1; i <= num; i++)
         {
 
             factorial = factorial * i;
-            
-            //1x1 factorial = 1
-            //1x2 factorial = 2
-            //2x3 factorial = 6
-            //6x4 factorial = 24
-            //24x5 factorial = 120
+
+            // 1x1 factorial = 1
+            // 1x2 factorial = 2
+            // 2x3 factorial = 6
+            // 6x4 factorial = 24
+            // 24x5 factorial = 120
 
         }
 
