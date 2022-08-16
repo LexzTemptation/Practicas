8ba99f7375f81550f69a71904e047b6ab2e80653
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-16 10:34:30 -0500

    SQL de gimnasio y queda acabarlo, también se ha modificado el diagrama ER

--- Programas/Gimnasio/gimnasioBD.sql
+++ Programas/Gimnasio/gimnasioBD.sql
@@ -27,18 +27,55 @@ CREATE TABLE person
 (
     idPerson INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     namePerson VARCHAR (70) NOT NULL DEFAULT '',
-    lastName VARCHAR (70) NOT NULL DEFAULT '',,
+    lastName VARCHAR (70) NOT NULL DEFAULT '',
     gender VARCHAR (70) NOT NULL DEFAULT '',
     addressPerson VARCHAR (200) NOT NULL DEFAULT '',
     phone VARCHAR (15) NOT NULL DEFAULT ''
 );
 
+CREATE TABLE members
+(
+	idMember  INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	
+	idPerson INT NOT NULL,
+	idClass INT NOT NULL,
+	
+	PRIMARY KEY (idMember, idPerson, idClass),
+	FOREIGN KEY (idPerson) REFERENCES person (idPerson),
+	FOREIGN KEY (idClass) REFERENCES classes (idClass)
+);
 
+CREATE TABLE instructor
+(
+	idInstructor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	degree TEXT DEFAULT NULL,
+	
+	idPerson INT NOT NULL,
+	idClass INT NOT NULL,
+	
+	FOREIGN KEY (idPerson) REFERENCES person (idPerson),
+	FOREIGN KEY (idClass) REFERENCES classes (idClass)
+);
 
+CREATE TABLE classes
+(
+	idClass INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	description TEXT DEFAULT NULL,
+	
+	idRoom INT NOT NULL,
+	idInstructor INT NOT NULL,
+	
+	FOREIGN KEY (idRoom) REFERENCES rooms (idPerson),
+	FOREIGN KEY (idInstructor) REFERENCES instructor (idInstructor)
+)
 
-
-
-
+CREATE TABLE courtSquash
+(
+	idCourt INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	locationCourt TEXT DEFAULT NULL,
+	courtStatus TEXT DEFAULT NULL,
+	reservation TEXT DEFAULT NULL
+)
 
 
 
