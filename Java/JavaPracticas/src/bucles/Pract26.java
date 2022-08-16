8f58f07804ce08e4d922ef44ed56feb052ff7355
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-16 14:32:13 -0500

    Agregada 'Pract26'

---
+++ Java/JavaPracticas/src/bucles/Pract26.java
@@ -0,0 +1,14 @@
+
+package bucles;
+
+public class Pract26
+{
+    public static void main(String[] args)
+    { 
+        System.out.println();
+        
+        
+
+        System.out.println();
+    }
+}
