e8fc5c36f2a5422f3f74778ca10f519d24c9815e
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-18 14:46:53 -0500

    Pract26 comentada por que daba error la variable booleana Teclear "ctrl + shift + /" sobre las lineas comentadas para quitar las diagonales de comentario

--- Java/JavaPracticas/src/bucles/Pract26.java
+++ Java/JavaPracticas/src/bucles/Pract26.java
@@ -4,37 +4,37 @@
 
 package bucles;
 
-import javax.swing.JOptionPane;
+// import javax.swing.JOptionPane;
 
-public class Pract26
-{
-    public static void main(String[] args)
-    { 
-        System.out.println();
+// public class Pract26
+// {
+//     public static void main(String[] args)
+//     { 
+//         System.out.println();
         
-        int calificacion = 0, contador = 0;
-        boolean suspensos = false;
+//         int calificacion = 0, contador = 0;
+//         boolean suspensos = false;
 
-        for (int i = 1; i <= 5; i++)
-        {
-            calificacion = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la calificación " + i));
+//         for (int i = 1; i <= 5; i++)
+//         {
+//             calificacion = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la calificación " + i));
 
-            if (calificacion <= 5)
-            {
-                suspensos = true; // si la condición se cumple pasa por este filtro y el contador aumenta de 1 en 1
-                contador++;
-            }
-        }
+//             if (calificacion <= 5)
+//             {
+//                 suspensos = true; // si la condición se cumple pasa por este filtro y el contador aumenta de 1 en 1
+//                 contador++;
+//             }
+//         }
 
-        if (suspensos = true)
-        {
-            JOptionPane.showMessageDialog(null,"Hay " + contador + " persona(s) en suspención");
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(null,"No hay suspensos");
-        }
+//         if (suspensos = true)
+//         {
+//             JOptionPane.showMessageDialog(null,"Hay " + contador + " persona(s) en suspención");
+//         }
+//         else
+//         {
+//             JOptionPane.showMessageDialog(null,"No hay suspensos");
+//         }
 
-        System.out.println();
-    }
-}
+//         System.out.println();
+//     }
+// }
