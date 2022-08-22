f0914a6dbf2792853bdeca601e020c1e6cead158
LexzTemptation <alejandro_tamptation@outlook.com>
2022-08-22 15:41:06 -0500

    Spring, jodido html

--- Programas/Spring/src/main/java/com/athenatechnologies/core/controller/ControllerBasic.java
+++ Programas/Spring/src/main/java/com/athenatechnologies/core/controller/ControllerBasic.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hey")
+@RequestMapping("/home")
 public class ControllerBasic
 {
-	@GetMapping(path = {"/saludo","/heyxd"})
+	@GetMapping(path = {"/post","/"})
 	public String saludo()
 	{
 		return "index";
