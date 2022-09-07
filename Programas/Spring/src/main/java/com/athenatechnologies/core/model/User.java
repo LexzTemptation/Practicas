5ff96134359260145d58bda085356575fc98ae13
LexzTemptation <alejandro_tamptation@outlook.com>
2022-09-07 16:00:46 -0500

    Corregidas anotaciones de no uso

--- Programas/Spring/src/main/java/com/athenatechnologies/core/model/User.java
+++ Programas/Spring/src/main/java/com/athenatechnologies/core/model/User.java
@@ -4,5 +4,25 @@ public class User
 {
     private String name, lastName;
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
     
 }
