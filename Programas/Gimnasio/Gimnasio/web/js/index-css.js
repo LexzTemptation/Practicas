f201976abf0c9670558a7d2fb985fce67fde5065
LexzTemptation <alejandro_tamptation@outlook.com>
2022-09-14 16:54:48 -0500

    Agregado el navbar transparente

--- Programas/Gimnasio/Gimnasio/web/js/index-css.js
+++ Programas/Gimnasio/Gimnasio/web/js/index-css.js
@@ -1,5 +1,22 @@
-date = new Date();
-year = date.getFullYear();
-month = date.getMonth() + 1;
-day = date.getDate();
-document.getElementById("current_date").innerHTML = year;
+
+//año
+//{
+    date = new Date();
+    year = date.getFullYear();
+    month = date.getMonth() + 1;
+    day = date.getDate();
+    document.getElementById("current_date").innerHTML = year;
+//}
+
+var nav = document.querySelector('nav');
+window.addEventListener('scroll', function()
+{
+    if (window.pageYOffset > 650)
+    {
+        nav.classList.add("bg-info", "shadow");
+    }
+    else
+    {
+        nav.classList.remove("bg-info", "shadow");
+    }
+});
