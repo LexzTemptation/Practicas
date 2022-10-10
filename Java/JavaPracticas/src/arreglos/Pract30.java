5dc2f4c1c8715f0dd2521095b744320fe04e0a6e
LexzTemptation <alejandro_tamptation@outlook.com>
2022-10-10 14:30:26 -0500

    Pract30 que la neta nmms no le supe hacer k-on

--- Java/JavaPracticas/src/arreglos/Pract30.java
+++ Java/JavaPracticas/src/arreglos/Pract30.java
@@ -1,4 +1,11 @@
 
+/*
+ * Pedir 12 nuemros para dos tablas, guardar esos numeros en dos arrys, y
+ * mezclarlos en un tercer array
+ * de la siguiente forma: 3 numeros del primero,3 del segundo, 3 del primero, 3
+ * desl segundo etc ...
+ */
+
 package arreglos;
 
 import java.util.Scanner;
@@ -10,7 +17,44 @@ public class Pract30
         System.out.println();
         Scanner feli = new Scanner(System.in);
 
+        int a[] = new int[12];
+        int b[] = new int[12];
+        int c[] = new int[24];
+        int j = 0;
+
+        for (int i = 0; i < 12; i++)
+        {
+            System.out.print((i + 1) + ".-Ingrese el número " + (i + 1) + " del arreglo A: ");
+            a[i] = feli.nextInt();
+        }
+
+        System.out.println("");
+
+        for (int i = 0; i < 12; i++)
+        {
+            System.out.print((i + 1) + ".-Ingrese el número " + (i + 1) + " del arreglo B: ");
+            b[i] = feli.nextInt();
+        }
+
         System.out.println();
+
+        for (int i = 0; i < 12; i++)
+        {
+            c[j] = a[i];
+            c[j+3] = b[i];
+            j++;
+
+            if (j == 3 || j == 9 || j == 15)
+            {
+                j += 3;
+            }
+        }
+
+        System.out.println("Orden: ");
+        for (int i = 0; i < 24; i++)
+        {
+            System.out.print(c[i] + " ");
+        }
 
         feli.close();
         System.out.println();
