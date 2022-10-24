08ed9758cc11a00145a62228bbfcbb14df0216d0
LexzTemptation <alejandro_temptation@outlook.com>
2022-10-24 14:04:12 -0500

    Agregando carpeta para los proyectos de spring - spring boot

---
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdem1/src/main/java/com/athtec/SpringUdem1Application.java
@@ -0,0 +1,13 @@
+package com.athtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUdem1Application.class, args);
+	}
+
+}
---
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdem1/src/test/java/com/athtec/SpringUdem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
