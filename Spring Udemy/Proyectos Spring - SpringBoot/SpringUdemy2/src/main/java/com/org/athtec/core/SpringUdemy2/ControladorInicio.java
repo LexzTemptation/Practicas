3165cc779949ef70a20ba82128563a931eeda100
LexzTemptation <alejandro_tamptation@outlook.com>
2022-11-01 15:18:02 -0600

    Cambio de nombres

--- Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy2/src/main/java/com/org/athtec/core/SpringUdemy2/ControladorInicio.java
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy2/src/main/java/com/org/athtec/core/SpringUdemy2/ControladorInicio.java
@@ -1,10 +1,10 @@
 /*2*/
-package com.org.athtec.core.SpringUdemy1;
+package com.org.athtec.core.SpringUdemy2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.extern.slf4j.Slf4j;
 
 @Controller /* */
 @Slf4j /* Simple Logging Facade for Java (abreviado SLF4J) actúa como una fachada para
---
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy2/src/main/java/com/org/athtec/core/SpringUdemy2/SpringUdemy2Application.java
@@ -0,0 +1,17 @@
+/* En este proyecto se trabajo con tecnología Thymeleaf */
+
+package com.org.athtec.core.SpringUdemy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdemy2Application
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringUdemy2Application.class, args);
+	}
+
+}
