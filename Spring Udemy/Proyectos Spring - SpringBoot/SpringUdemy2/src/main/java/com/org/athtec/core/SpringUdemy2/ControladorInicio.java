ed9ff7610621c702d9ba5cbe2e3041bc5128d967
LexzTemptation <alejandro_tamptation@outlook.com>
2022-11-04 13:14:06 -0600

    Implementación de la clase persona

--- Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy2/src/main/java/com/org/athtec/core/SpringUdemy2/ControladorInicio.java
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy2/src/main/java/com/org/athtec/core/SpringUdemy2/ControladorInicio.java
@@ -16,10 +16,19 @@ import org.springframework.web.bind.annotation.GetMapping;
                 independiente de la implementación real. */
 public class ControladorInicio
 {
-
-    @Value("${index.saludo}")
+    /*  Para poder acceder a la informacion de "application.properties" especificamente a 
+        la llave valor, se utiliza la anotación "@Value", ATENCIÓN EN ESTO, ya que si se
+        anota otra cosa referido a lombok, la applicación no va a funcionar.
+    */
+    @Value("${index.saludo}") /* Spring lo que hace es aplicar el mismo concepto de inyección de dependencias,
+                                así que debido a que la información de application.properties ya está disponible
+                                para el contenedor de Spring, lo que hace Spring es buscar de manera automatica 
+                                la llave "${index.saludo}" utilizando la sintaxis y toma el valor relacionado a 
+                                esta llave y lo inyecta como valor de la variable "saludo".
+                               */
     private String saludo;
 
+    /*  Con lo siguiente se está compartiendo información del MODELO hacia la VISTA */
     @GetMapping("/")
     public String inicio(Model model)/* Aquí se recibía el objero http servlet request y response, y utlizando
                                         uno de los alcances como "request", "session" o "application" se compartian
@@ -30,10 +39,20 @@ public class ControladorInicio
     {
         var mensaje = "Fierro otra vez 2";
 
+        /* Se crea una variable del tipo persona e inicializamos cada uno de sus valores 
+         * y se comparte la información utilizando la clase de model, compartiendo el objeto de persona.
+        */
+        var persona = new Persona();
+        persona.setNombre("Lexz");
+        persona.setApellido("Temptation");
+        persona.setEmail("lexz_temptation@outlook.com");
+        persona.setTelefono("4776712536");
+
         log.info("Ejecutando controlador Spring MVC");
         /* return "html/index.html"; */
         model.addAttribute("mensaje", mensaje); /* ("llave", valorDeLaVariable) En este caso la variable es mensaje */
-        model.addAttribute("saludo", saludo);
+        model.addAttribute("saludo", saludo); 
+        model.addAttribute("persona", persona);
         return "index";
 
 
