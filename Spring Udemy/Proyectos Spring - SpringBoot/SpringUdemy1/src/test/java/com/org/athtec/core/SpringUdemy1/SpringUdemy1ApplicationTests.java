f654bf97158a35ddc57401cc8fc1b8d8c81e2879
LexzTemptation <alejandro_tamptation@outlook.com>
2022-11-10 14:01:25 -0600

    agregado de carpetas de spring udemy

---
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy1/src/test/java/com/org/athtec/core/SpringUdemy1/SpringUdemy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.athtec.core.SpringUdemy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
