b0f59b9fc255bf71a7eaffa7278a74d99ce26888
LexzTemptation <alejandro_tamptation@outlook.com>
2022-11-10 13:46:44 -0600

    Revert "Cambios extraños hay en el proyecto"
    This reverts commit 20609efa533ed1077a8419c0fa0d3e970bdd8f94.

--- Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy3SpringData/src/main/java/com/athtec/core/SpringUdemy3/dao/PersonaDao.java
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy3SpringData/src/main/java/com/athtec/core/SpringUdemy3/dao/PersonaDao.java
@@ -5,11 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.org.athtec.core.SpringUdemy3.domain.Persona;
 
-@Transactional
 @Service
 @Component
 @Repository
