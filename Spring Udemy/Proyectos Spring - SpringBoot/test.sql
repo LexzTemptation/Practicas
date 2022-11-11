22e0a79fbc846263ce5eece0f0e474fc4fee0967
LexzTemptation <alejandro_tamptation@outlook.com>
2022-11-10 21:24:09 -0600

    Agregado sql de test

---
+++ Spring Udemy/Proyectos Spring - SpringBoot/test.sql
@@ -0,0 +1,43 @@
+
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE persona
+(
+    id_persona INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(45),
+    apellido VARCHAR(45),
+    email VARCHAR(45),
+    telefono VARCHAR(45)
+);
+
+INSERT INTO persona VALUES 
+(
+    1010, 
+    "Juan", 
+    "Perez", 
+    "jpear@gmail.com", 
+    "1234567891"
+);
+
+INSERT INTO persona VALUES 
+(
+    default, 
+    "Ramiro", 
+    "Leata", 
+    "rleata@gmail.com", 
+    "987654321"
+);
+
+INSERT INTO persona VALUES 
+(
+    default, 
+    "Martina", 
+    "Lawrence", 
+    "mlaw@outlook.com", 
+    "38456583"
+);
+
+
+
