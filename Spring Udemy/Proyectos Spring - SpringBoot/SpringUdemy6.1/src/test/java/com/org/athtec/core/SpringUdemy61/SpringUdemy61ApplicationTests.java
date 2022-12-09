99a6e193717e05ddd7554124ee754696e9aea637
LexzTemptation <alejandro_tamptation@outlook.com>
2022-12-08 19:01:53 -0600

    Proyecto 6.1

---
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy6.1/src/test/java/com/org/athtec/core/SpringUdemy61/SpringUdemy61ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.athtec.core.SpringUdemy61;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemy61ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
