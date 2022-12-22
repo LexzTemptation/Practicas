f423b37dbee5e24c8f8b20c388371fe27b97fb9d
LexzTemptation <alejandro_tamptation@outlook.com>
2022-12-22 13:03:08 -0600

    Implementación de internacionalización al proyecto SpringUdemy6.2, y agregación del proyecto SpringUdemy7.

--- Java/JavaExample/src/EjercicioDePractica2.java
+++ Java/JavaExample/src/EjercicioDePractica2.java
@@ -1,7 +1,23 @@
+
+import java.util.Scanner;
+
 public class EjercicioDePractica2
 {
     public static void main(String[] args)
     {
+        System.out.println();
+        Scanner feli = new Scanner(System.in);
 
+        double volumen, lado;
+
+        System.out.print("Ingrese la medida de uno de los lados del cubo: ");
+        lado = feli.nextFloat();
+
+        volumen = Math.pow(lado, 3);
+
+        System.out.print("El volumen es: " + volumen);
+
+        feli.close();
+        System.out.println();
     }
 }
