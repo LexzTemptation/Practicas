c68cf037bf9a88e5c287ff548baed3a6571d8263
Alejandro Cornejo <alejandro_temptation@outlook.com>
2022-12-31 14:34:03 -0600

    Formato correcto de la clase SecurityConfig.java

--- Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy7.1/src/main/java/com/org/athtec/SpringUdemy71/web/SecurityConfig.java
+++ Spring Udemy/Proyectos Spring - SpringBoot/SpringUdemy7.1/src/main/java/com/org/athtec/SpringUdemy71/web/SecurityConfig.java
@@ -10,24 +10,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
 
-   /* Este método recibe un objeto de "AutenticationManagerBuilder",
-      el cual sirve para agregar más usuarios y personalizarlos para
-      hacer loggin en la aplicación */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-      auth.inMemoryAuthentication()
-          .withUser("admin")
-            .password("{noop}1234") /* De esta manera no se encripta el password*/
-            .roles("ADMIN","USER")
-          .and()
-          .withUser("user")
-            .password("{noop}1234")
-            .roles("USER")
-          ;
-   }
-   /* Este método recibe un parameto de "http security", el cual se va
-      a utilizar para restringir las url's de la aplicación web a otros
-      usuarios que no estén autorizados a entrar a ellas y según el
-      rol que se le haya asignado */
+	/* Este método recibe un objeto de "AutenticationManagerBuilder",
+	   el cual sirve para agregar más usuarios y personalizarlos para
+	   hacer loggin en la aplicación */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	{
+		auth.inMemoryAuthentication()
+				.withUser("admin")
+					.password("{noop}1234") /* De esta manera no se encripta el password */
+					.roles("ADMIN", "USER")
+				.and()
+				.withUser("user")
+					.password("{noop}1234")
+					.roles("USER");
+	}
+	/* Este método recibe un parameto de "http security", el cual se va
+	   a utilizar para restringir las url's de la aplicación web a otros
+	   usuarios que no estén autorizados a entrar a ellas y según el
+	   rol que se le haya asignado */
 }
