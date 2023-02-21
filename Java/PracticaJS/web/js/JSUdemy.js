3cc83b212ec273a1130997d13a3e72bb977dc3ff
LexzTemptation <alejandro_temptation@outlook.com>
2023-02-20 22:42:38 -0600

    JS Udemy

---
+++ Java/PracticaJS/web/js/JSUdemy.js
@@ -0,0 +1,20 @@
+
+let saludo = "Hey prro";
+console.log(saludo);
+console.log("Hey prro");
+
+let numero = 222;
+console.log(numero);
+
+// Tipo de dato object
+let persona = 
+{
+    nombre : "Lexz",
+    apellido : "Temp",
+    telefono : 4776712536
+}
+
+console.log(persona);
+
+
+
