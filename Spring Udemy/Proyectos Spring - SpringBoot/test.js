dbaa6b626abf2eef6689fa8c142309981d04c1d8
LexzTemptation <alejandro_temptation@outlook.com>
2023-03-06 13:19:50 -0600

    No c

--- Spring Udemy/Proyectos Spring - SpringBoot/test.js
+++ Spring Udemy/Proyectos Spring - SpringBoot/test.js
@@ -1,4 +1,11 @@
 db.counters.insert
+<<<<<<< HEAD
+(
+   {
+      _id: "id_persona",
+      seq: 1009
+   }
+=======
 
 (
 
@@ -10,6 +17,7 @@ db.counters.insert
 
    }
 
+>>>>>>> fc936fe89239a358fc9de7e26cc7b0350746a216
 )
 
 
@@ -17,6 +25,19 @@ db.counters.insert
 /*Antes de ingresar un nuevo registro se tiene que convocar esta funci�n*/
 
 function getNextSequence(id_persona) 
+<<<<<<< HEAD
+{
+   var ret = db.counters.findAndModify
+    (
+          {
+            query: { _id: id_persona },
+            update: { $inc: { seq: 1 } },
+            new: true,
+            upsert: true
+          }
+   );
+   return ret.seq;
+=======
 
 {
 
@@ -46,11 +67,30 @@ function getNextSequence(id_persona)
 
    return ret.seq;
 
+>>>>>>> fc936fe89239a358fc9de7e26cc7b0350746a216
 }
 
 
 
 function insertDocument(doc, targetCollection) 
+<<<<<<< HEAD
+{
+    while (1)
+        {
+            var cursor = targetCollection.find( {}, { _id: 1 } ).sort( { _id: -1 } ).limit(1);
+            var seq = cursor.hasNext() ? cursor.next()._id + 1 : 1;
+            doc._id = seq;
+            var results = targetCollection.insert(doc);
+            if( results.hasWriteError() ) 
+                {
+                    if( results.writeError.code == 11000 /* dup key */ )
+                    continue;
+                    else
+                    print( "unexpected error inserting data: " + tojson( results ) );
+                }
+        break;
+    }
+=======
 
 {
 
@@ -96,6 +136,7 @@ function insertDocument(doc, targetCollection)
 
     }
 
+>>>>>>> fc936fe89239a358fc9de7e26cc7b0350746a216
 }
 
 
@@ -103,91 +144,89 @@ function insertDocument(doc, targetCollection)
 /*datos de las personas*/
 
 db.persona.insert
-
 (
-
     {
-
         _id: getNextSequence("id_persona"),
-
         nombre : "Juan",
-
         apellido : "Perez",
-
         email : ('jpear@gmail.com '),
-
     	telefono : "1234567891"
-
     }
-
 )
 
 
 
 db.persona.insert
-
 (
-
     {
-
         _id: getNextSequence("id_persona"),
-
         nombre : "Ramiro",
-
         apellido : "Leata",
-
         email : ('rleata@gmail.com '),
-
+<<<<<<< HEAD
     	telefono : "987654321"
-
     }
+)
 
+=======
+    	telefono : "987654321",
+        direccion: 
+        {
+            "calle": "Colon",
+            "numero": 1200,
+            "codigopostal": 5000
+        }
+    }
 )
 
     
 
+>>>>>>> fc936fe89239a358fc9de7e26cc7b0350746a216
 db.persona.insert
-
 (
-
     {
-
         _id: getNextSequence("id_persona"),
-
         nombre : "Martina",
-
         apellido : "Lawrence",
-
         email : ('mlaw@gmail.com '),
-
+<<<<<<< HEAD
     	telefono : "38456583"
-
+=======
+    	telefono : "38456583",
+        direccion: 
+        {
+            calle: "Dean Funes",
+            numero: 23,
+            codigopostal: 5002
+        }
+>>>>>>> fc936fe89239a358fc9de7e26cc7b0350746a216
     }
-
 )
 
 
 
 db.persona.insert
-
 (
-
     {
-
         _id: getNextSequence("id_persona"),
-
         nombre : "Paola",
-
         apellido : "Fran",
-
         email : ('pfra@gmail.com '),
-
+<<<<<<< HEAD
     	telefono : "56345347"
-
     }
-
+) 
+=======
+    	telefono : "56345347",
+        "direccion": 
+        {
+            "calle": "Colon",
+            "numero": 150,
+            "codigopostal": 5000
+        }
+    }
 ) 
 
    
 
-show databases
+>>>>>>> fc936fe89239a358fc9de7e26cc7b0350746a216
