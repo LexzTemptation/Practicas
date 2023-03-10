1f540b5ac6891c283a12698bf3bbeacfa89387e5
LexzTemptation <alejandro_temptation@outlook.com>
2023-03-10 12:43:17 -0600

    weas de react

--- Java/ReactJS/react-to-do/src/index.js
+++ Java/ReactJS/react-to-do/src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Yeah!!</div>, document.getElementById("root"));
+ReactDOM.render(<div>Zhy!!</div>, document.getElementById("root"));
