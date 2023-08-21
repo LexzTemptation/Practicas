fb6a54dfe7a6e3aa16959c19e82705127bba424d
LexzTemptation <alejandro_temptation@outlook.com>
2023-08-21 12:40:37 -0600

    Cambios

---
+++ Java/YabaEscrit/Curso JS/Ejercicios/1/js/index.js
@@ -0,0 +1,57 @@
+/* let edad = parseInt(prompt("¿Cual es tu edad?"));
+
+if (edad >= 18) {
+    document.write(`<h1>Tu edad es ${edad}</h1>`);
+    Swal.fire({
+        icon: "success",
+        title: `<h3>Eres mayor de edad</h3>`,
+    });
+} else {
+    document.write(`<h1>Tu edad es ${edad} <i class="fa-solid fa-ban fa-flip" style="color: #e81111;"></i></h1>`);
+    Swal.fire({
+        icon: "error",
+        title: "",
+        title: `<h3>Eres menor de edad <i class="fa-solid fa-ban fa-flip" style="color: #e81111;"></i></h3>`,
+    });
+} */
+
+/* (async () => {
+    const { value: number } = await Swal.fire({
+        icon: "question",
+        title: `¿Cual es tu edad?`,
+        input: "number",
+    });
+
+    if (number >= 18) {
+        Swal.fire({
+            icon: "success",
+            title: `<h3>Eres mayor de edad</h3>`,
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Eres menor de edad",
+        });
+    }
+})(); */
+
+/* do{
+
+    let calf = parseInt(prompt('Ingresa tu calificación'));
+
+    if(calf > 0 && calf < 10)
+    {
+        if(calf === 0 || calf <= 3)
+        {
+            alert('Tu calificación es muy deficiente')
+        }
+        else if (calf >= 3 && calf < 5)
+        {
+
+        }
+    }
+
+
+
+}
+while(calf != undefined); */
